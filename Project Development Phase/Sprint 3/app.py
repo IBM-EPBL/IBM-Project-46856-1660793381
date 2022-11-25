e89c4685964fc2e46d8c7e490c7e184c81f9d2f8
Dayanand <danishboss022@gmail.com>
2022-11-25 22:58:51 +0530

    final

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -6,7 +6,6 @@ import email_validator
 from functools import wraps
 import re
 import ibm_db
-import sqlite3 as sql
 from datetime import datetime
 
 conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30376;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=ywz69948;PWD=bpABt6nFhm0Fzkqg", '', '')
